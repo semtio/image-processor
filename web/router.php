996c03d76b68ba7d557ab9537c1368ffe4c1abd8
semtio <semik20092@gmail.com>
2026-01-06 13:35:16 +0100

    Упрощено определение маршрутов: улучшена обработка API запросов и статических файлов

--- web/router.php
+++ web/router.php
@@ -5,14 +5,18 @@
  * Routes API requests to appropriate handlers
  */
 
-// Get request path
-$request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Routes handling
-if (strpos($request_path, '/api/') !== false) {
+// Check if this is an API request
+if (strpos($_SERVER['REQUEST_URI'], '/api/') !== false) {
     // API requests
     require __DIR__ . DIRECTORY_SEPARATOR . 'api.php';
-} else {
-    // Serve index.html for all other requests
-    require __DIR__ . DIRECTORY_SEPARATOR . 'index.html';
+    exit;
 }
+
+// Serve static files
+$path = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (is_file($path)) {
+    return false; // Let PHP serve the file
+}
+
+// Default to index.html for all other requests
+require __DIR__ . DIRECTORY_SEPARATOR . 'index.html';
